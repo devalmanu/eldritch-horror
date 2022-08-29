e919196ff58499dbe9b1ac67cdd713a5e3b4d950
Valiantsina Andreyeva <manuilova-1986@mail.ru>
2022-08-29 11:23:39 +0300

    fix: file links card

--- data/ancients.js
+++ data/ancients.js
@@ -4,7 +4,7 @@ const ancientsData = [
   {
     id: 'azathoth',
     name: 'azathoth',
-    cardFace: '../assets/Ancients/Azathoth.png',
+    cardFace: './assets/Ancients/Azathoth.png',
     firstStage: {
       greenCards: 1,
       brownCards: 2,
@@ -24,7 +24,7 @@ const ancientsData = [
   // {
   //   id: 'cthulhu',
   //   name: 'cthulhu',
-  //   cardFace: '../assets/Ancients/Cthulthu.png',
+  //   cardFace: './assets/Ancients/Cthulthu.png',
   //   firstStage: {
   //     greenCards: 0,
   //     brownCards: 2,
@@ -44,7 +44,7 @@ const ancientsData = [
   // {
   //   id: 'iogSothoth',
   //   name: 'iogSothoth',
-  //   cardFace: '../assets/Ancients/IogSothoth.png',
+  //   cardFace: './assets/Ancients/IogSothoth.png',
   //   firstStage: {
   //     greenCards: 0,
   //     brownCards: 2,
@@ -64,7 +64,7 @@ const ancientsData = [
   // {
   //   id: 'shubNiggurath',
   //   name: 'shubNiggurath',
-  //   cardFace: '../assets/Ancients/ShubNiggurath.png',
+  //   cardFace: './assets/Ancients/ShubNiggurath.png',
   //   firstStage: {
   //     greenCards: 1,
   //     brownCards: 2,
